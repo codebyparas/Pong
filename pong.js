54332e6b6a46c7fdcc3bb2421539e6c9480cf9d1
Paras-Sachdeva <paras140902@gmail.com>
2024-05-25 23:08:08 +0530

    ADD: Initial Player 1

--- pong.js
+++ pong.js
@@ -4,9 +4,24 @@ let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
+// Players
+let playerWidth = 10;
+let playerHeight = 50;
+
+let player1 = {
+    x : 10,
+    y : boardHeight/2,
+    width : playerWidth,
+    height : playerHeight
+}
+
 window.onload = function(){
     board= document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");  // Used for Drawing on the Board
+
+    // Draw Initial Player1
+    context.fillStyle = "skyblue";
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
 }
